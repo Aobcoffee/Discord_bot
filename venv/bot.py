bdabe24d756ffe2903a9efccc51f40ac40096b52
Aobcoffee <aobgambit@gmail.com>
2023-10-20 16:27:49 +0800

    Update bot.py
    ADD: prefix for bot to get dms

--- venv/bot.py
+++ venv/bot.py
@@ -34,6 +34,12 @@ def run_discord_bot():
         channel = str(message.channel)
         is_private = isinstance(message.channel, discord.DMChannel)
         
+        # Initialize with character "?" (can be any prefix) to get directed message
+        if user_message and user_message[0] == "?":
+            user_message = user_message[1:] # removing the "?" character
+            await send_message(message, user_message, is_private = True)
+        elif user_message:
+            await send_message(message, user_message, is_private = False)
 
         await send_message(message, user_message, is_private = False)
 
